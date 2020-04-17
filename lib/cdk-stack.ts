3d3a90c855d3d5c21b91489043fda6c8d27497ca
Bryan S <bstouffer@gmail.com>
2020-04-17 13:24:58 -0400

    Initial cdk commit.

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,18 +1,33 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import lambda = require('@aws-cdk/aws-lambda');
+import path = require('path');
 import {Tag} from "@aws-cdk/core";
 
 export class CdkStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		let hostingBucket = new s3.Bucket(this, 'HostingBucket');
+		let hostingBucket = new s3.Bucket(this, 'HostingBucket', {
+			websiteIndexDocument: 'index.html',
+			publicReadAccess: true
+		});
 		Tag.add(hostingBucket, 'test', 'bryan');
 
 		const resource = hostingBucket.node.findChild('Resource') as cdk.CfnResource;
 		resource.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
+		new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
+			sources: [s3Deployment.Source.asset('./vue-build')],
+			destinationBucket: hostingBucket,
+			destinationKeyPrefix: 'web/static' // optional prefix in destination bucket
+		});
 
-		// The code that defines your stack goes here
+		const lambdaTest = new lambda.Function(this, 'LambdaTest', {
+			runtime: lambda.Runtime.NODEJS_10_X,
+			handler: 'index.handler',
+			code: lambda.Code.fromAsset(path.join(__dirname, '../lambdaTest')),
+		});
 	}
 }
