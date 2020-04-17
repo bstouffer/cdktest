fa191733b0add066bd3e8c4695eb59ed8804bdc7
Bryan S <bstouffer@gmail.com>
2020-04-17 12:07:40 -0400

    Initial cdk commit.

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+require('dotenv').config();
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new CdkStack(app, 'CsbRootStack-' + process.env.APP_ENV);
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,9 +1,18 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import {Tag} from "@aws-cdk/core";
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+		let hostingBucket = new s3.Bucket(this, 'HostingBucket');
+		Tag.add(hostingBucket, 'test', 'bryan');
+
+		const resource = hostingBucket.node.findChild('Resource') as cdk.CfnResource;
+		resource.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+
+		// The code that defines your stack goes here
+	}
 }
